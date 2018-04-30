345064c6f7d544f382ddc09387283ca56c9bbe64
israelmurilocadorin <37721806+israelmurilocadorin@users.noreply.github.com>
2018-04-30 14:00:43 -0300

    Add files via upload

---
+++ exer3..cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main(){
+	
+	int x1, x2;
+	setlocale(LC_ALL,"");
+	
+	printf("Digite um inteiro: ");
+	scanf("%d", &x1);
+	
+	printf("Digite um inteiro: ");
+	scanf("%d", &x2);
+	
+	x1 = x1 + 1;
+	x2 = x2 - 1;
+	
+	while(x1 <= x2)
+	{
+		printf("\n\n [%d]", x1);
+		x1++;
+	}
+	
+	printf("\n\n");
+	system("pause");
+	return 0;
+}
